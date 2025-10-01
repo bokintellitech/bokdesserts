164624ad85d185b1054f5bb2722ab7f83fd14f6e
bokintellitech <bokintellitech@gmail.com>
2025-10-01 11:30:37 -0600

    Update cart-sheet.tsx

--- components/cart-sheet.tsx
+++ components/cart-sheet.tsx
@@ -10,14 +10,14 @@ import { Separator } from "@/components/ui/separator"
 import Link from "next/link"
 
 export function CartSheet() {
-  const { cart, updateQuantity, removeFromCart, getTotal, getItemCount } = useCart()
+  const { cart, updateQuantity, removeFromCart, getTotal, getItemCount, isLoaded } = useCart()
 
   return (
     <Sheet>
       <SheetTrigger asChild>
         <Button size="icon" variant="ghost" className="relative">
           <ShoppingCart className="h-5 w-5" />
-          {getItemCount() > 0 && (
+          {isLoaded && getItemCount() > 0 && (
             <Badge className="absolute -top-1 -right-1 h-5 w-5 rounded-full p-0 flex items-center justify-center text-xs">
               {getItemCount()}
             </Badge>
