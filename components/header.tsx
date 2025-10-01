127814fae7adabae41fb4013cf840688f4cb7db5
bokintellitech <bokintellitech@gmail.com>
2025-10-01 12:22:57 -0600

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -26,7 +26,7 @@ export function Header() {
           <div className="flex h-10 w-10 items-center justify-center rounded-xl bg-primary">
               <Link href="/" className="flex items-center gap-2">
                  <Image 
-                  src="/logo.png" 
+                  src="/bok_logo.png" 
                   alt="BokDesserts Logo" 
                   width={40} 
                   height={40}
